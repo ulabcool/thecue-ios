687a4d9b0701f4335a365498389c69b0e991d5a6
Adil Bougamza <adil.bougamza@gmail.com>
2017-07-28 16:00:57 +0200

    📦 create UsersListViewController

---
+++ thecue/UsersListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UsersListViewController.swift
+//  thecue
+//
+//  Created by Adil Bougamza on 28/07/2017.
+//  Copyright © 2017 Usabilla. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class  UsersListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        
+    }
+}
